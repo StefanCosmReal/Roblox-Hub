b02d8d7226eb21ae824e6808f2e0a2bb9a2325e0
StefanCosmReal <k1ngutz123@gmail.com>
2025-10-14 01:12:59 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -4,13 +4,40 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "AervanixBot (Rayfield)",
-   LoadingTitle = "AervanixBot",
-   LoadingSubtitle = "Rayfield UI",
-   ShowText = "Aervanix",
-   Theme = "Ocean",
-   ToggleUIKeybind = "K",
-   ConfigurationSaving = { Enabled = true, FolderName = "Aervanix", FileName = "config" },
+   Name = "Multi tool | Made by StefanCosm_",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Getting scripts ready for you",
+   LoadingSubtitle = "By StefanCosm_",
+   ShowText = "Multitool", -- for mobile users to unhide rayfield, change if you'd like
+   Theme = "Ocean", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Scripts"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Cheita",
+      Subtitle = "Key System",
+      Note = "Dm StefanCosm_ on Discord.", -- Use this to tell the user how to get a key
+      FileName = "Cheite", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {github.com/StefanCosmReal/Passwords/blob/main/Password} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
 })
 
 -- Services helper
@@ -776,5 +803,3 @@ return function()
     for p, _ in pairs(espMap) do espDetach(p) end
     if espGui and espGui.Parent then espGui:Destroy() end
 end
-
-local Tab = Window:CreateTab("Tab Example", 115565752692668) -- Title, Image
